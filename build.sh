88fcc69fe3b320f303791a45fbcf201f9b49c904
luvsunlight <516060659@qq.com>
2019-06-26 14:17:32 +0800

    update

--- build.sh
+++ build.sh
@@ -1,6 +1,14 @@
 defaultString="⭐️ update"
 
-# npm run build
+echo "\033[32m Start disting...\033[0m"
+
+echo "\033[32m Start building...\033[0m"
+
+npm run build
+
+echo "\033[32m End building...\033[0m"
+
+echo "\033[32m Start sync to github...\033[0m"
 
 git add .
 
@@ -10,3 +18,10 @@ read content
 
 git commit -m $content
 
+echo "\033[32m End sync to github...\033[0m"
+
+# git push origin master
+
+# npm login
+
+# npm publish
